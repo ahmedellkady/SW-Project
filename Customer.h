04df90d7d680d9675f4e5e1a85dd468a3382d8d3
AhmedEssam <ahmedezsam@gmail.com>
2025-11-27 22:30:36 +0200

    Updated Version

--- Customer.h
+++ Customer.h
@@ -1,21 +1,26 @@
-//
-// Created by Lenovo on 11/26/2025.
-//
-
 #ifndef CUSTOMER_H
 #define CUSTOMER_H
 #include "Movie.h"
+#include "ShowTime.h"
 #include "User.h"
-
+#include <memory>
 
 class Customer : public User {
 public:
-    Customer() = delete;
-    Customer(string e, string n, string p, int age) : User(e, n, p, age) {};
-    void bookSeat(Movie movie,ShowTime showTime);
-    void displayRole() override;
+    Customer(int id,
+             const std::string& email,
+             const std::string& name,
+             const std::string& password,
+             int age);
+
+    virtual ~Customer() = default;
+
+    virtual void displayRole() const override;
+
+    // book a seat in a given showtime of a movie
+    void bookSeat(const std::shared_ptr<Movie>& movie,
+                  const std::shared_ptr<ShowTime>& showTime,
+                  int seatIndex);
 };
-
-
 
 #endif //CUSTOMER_H
